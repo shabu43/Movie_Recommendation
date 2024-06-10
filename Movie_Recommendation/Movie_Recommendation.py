d040c2a9496f24d2e2c1526587f24d9dc8feb74d
SM Habibur Shabu <shabu@SMs-MacBook-Pro.local>
2024-06-11 01:51:37 +0200

    Movie_Recommendation: refining words to their root with porter-stemmer

--- Movie_Recommendation/Movie_Recommendation.py
+++ Movie_Recommendation/Movie_Recommendation.py
@@ -1,8 +1,22 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from nltk.stem import PorterStemmer
 
 movies_df = pd.read_csv("/Users/shabu/Desktop/Movie_Recommendation/databases/pre_processing_done.csv")
 
+
 CountVe = CountVectorizer(max_features=5000, stop_words='english')
 v = CountVe.fit_transform(movies_df["tags"]).toarray()
+porterS = PorterStemmer()
+
+def stemm(text):
+    x=[]
+    for i in text.split():
+        x.append(porterS.stem(i))
+    return " ".join(x)
+
+movies_df["tags"] = movies_df["tags"].apply(stemm)
+print(movies_df["tags"].head())
+
