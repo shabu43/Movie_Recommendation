fd200fecdad28985d123fe3321229f1c6d5e4946
SM Habibur Shabu <shabu@SMs-MacBook-Pro.local>
2024-06-11 21:00:41 +0200

    Movie_Recommendation: Done, Based on similarity matrix recommend movies

--- Movie_Recommendation/Movie_Recommendation.py
+++ Movie_Recommendation/Movie_Recommendation.py
@@ -25,8 +25,24 @@ movies_df["tags"] = movies_df["tags"].apply(stemm)
 #Find similarity between each movie entity using cosine_similarity
 v = CountVe.fit_transform(movies_df["tags"]).toarray()
 similarity = cosine_similarity(v)
-print(sorted(list(enumerate(similarity[0])), reverse=True, key = lambda x:x[1])[1:6])
+#print(sorted(list(enumerate(similarity[0])), reverse=True, key = lambda x:x[1])[1:6])
 
+#recomend movie based on similarities
+def recommend(title):
+    m_index = movies_df[movies_df['title'] == title].index[0]
+    distances = similarity[m_index]
+    mo_list = sorted(list(enumerate(distances)), reverse = True, key = lambda x:x[1])[1:7]
+    for i in mo_list:
+        print(movies_df.iloc[i[0]].title)
+        
+val = input("Enter a movie title: ")
 
-
+r=0
+for i in movies_df["title"]:
+    if i == val:
+        recommend(val)
+        r=1
+        break;
+if r == 0:
+        print("sorry!! the movoie is Not recognized by the system!!\nPlease insert a correct Title")
 
